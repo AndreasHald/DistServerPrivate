21fb1aec196762e24fea54e571592929c5654fa6
AndreasHald <andreas.boris.hald@gmail.com>
2014-10-27 17:06:38 +0100

    Fixet Server
    Server klasse virker, og tilgår GiantSwitch, encryption virker, og
    henter krypkey fra Configurations

--- src/ClientWorker.java
+++ src/ClientWorker.java
@@ -17,7 +17,7 @@ public class ClientWorker implements  Runnable{
 	
 	public void run(){
 		try{
-			System.out.println("forbindelse Oprettet!");
+			System.out.println("Client Connected:");
 			//BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 			byte[] b = new byte[500000];
 			int count = connectionSocketConected.getInputStream().read(b);
@@ -25,20 +25,17 @@ public class ClientWorker implements  Runnable{
 			DataInputStream inFromClient = new DataInputStream(connectionSocketConected.getInputStream());		
 			//Creates an object of the data which is to be send back to the client, via the connectionSocket
 			DataOutputStream outToClient = new DataOutputStream(connectionSocketConected.getOutputStream());
-			System.out.println("Outtoclient oprettet!");
 			//Sets client sentence equals input from client
 			//incomingJson = inFromClient.readLine();			
 			
+			// DECRYPT INCOMMING STRING
 			String ny = cryp.decrypt(b);
 			
-			//cryp.StringEncryption(inFromClient.readLine());
-			System.out.println("Besked modtaget!");
-			//Sysout recieved message
 			System.out.println("Received: " + ny);
+			
 			String returnSvar = GS.GiantSwitchMethod(ny);		
-			//Sends the capitalized message back to client!!
 			outToClient.writeBytes(returnSvar + "\n");
-			System.out.println("svar sendt");
+			System.out.println("Reply sent");
 			//BufferedWriter writer = new BufferedWriter(arg0)
 		}catch(Exception exception){
 			System.err.print(exception);
